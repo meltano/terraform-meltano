e900a4a020e2447535532e1ba934e69eb204e3db
kgpayne <ken@meltano.com>
2021-12-14 13:04:50 +0000

    more region fixing

--- aws/modules/infrastructure/variables.tf
+++ aws/modules/infrastructure/variables.tf
@@ -3,3 +3,8 @@ variable "aws_region" {
   description = "AWS region to deploy to."
   default     = "us-east-1"
 }
+
+variable "kubernetes_namespace" {
+  description = "Kubernetes namespace for Meltano and Airflow."
+  default     = "meltano"
+}
