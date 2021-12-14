efe678115b11efaf52ff0ce83480facd0b568f90
kgpayne <ken@meltano.com>
2021-12-14 17:23:42 +0000

    update provider requirements

--- aws/modules/infrastructure/provider.tf
+++ aws/modules/infrastructure/provider.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.65.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.6.1"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.4.1"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
--- kubernetes/modules/meltano/provider.tf
+++ kubernetes/modules/meltano/provider.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "2.1.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.6.1"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.4.1"
+    }
+  }
+}
+
 provider "kubernetes" {
   host                   = var.kubernetes_cluster_endpoint
   cluster_ca_certificate = var.kubernetes_cluster_ca_certificate
