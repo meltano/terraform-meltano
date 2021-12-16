cba5fcdff5492b744fd0a647eed2be6330fd45e6
Ken Payne <ken@meltano.com>
2021-12-16 11:26:41 +0000

    Moved to KubernetesPodOperator model.

--- kubernetes/modules/meltano/meltano.tf
+++ kubernetes/modules/meltano/meltano.tf
@@ -1,27 +1,22 @@
 locals {
-  meltano_values = <<EOT
-extraEnv: []
-
-meltano:
-  database_uri: ${var.meltano_db_uri}
-
-image:
-  repository: ${var.meltano_image_repository_url}
-  tag: ${var.meltano_image_tag}
-
-EOT
+  meltano_values = {
+    database_uri         = var.meltano_db_uri
+    image_repository_url = var.meltano_image_repository_url
+    image_tag            = var.meltano_image_tag
+    b64_env_file         = base64encode(var.meltano_env_file)
+  }
 }
 
 resource "helm_release" "meltano" {
   name       = "meltano"
   repository = "https://meltano.gitlab.io/infra/helm-meltano/meltano"
   chart      = "meltano"
-  namespace  = "meltano"
-  version    = "0.1.1"
-  wait       = false
+  # chart = "../../../infrastructure/helm-meltano/meltano"
+  namespace = "meltano"
+  version   = "0.2.0"
+  wait      = false
   values = [
-    file("${path.module}/meltano.values.yaml"),
-    local.meltano_values
+    templatefile("${path.module}/meltano.values.tftpl", local.meltano_values)
   ]
   # This is not a chart value, but just a way to trick helm_release into running every time.
   # Without this, helm_release only updates the release if the chart version (in Chart.yaml) has been updated
